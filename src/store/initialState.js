7c28c76d23c4e290d31f28590757554eaf10d888
thureson <tom.thureson@gmail.com>
2018-09-09 21:00:35 +0300

    Make initialState compact.

--- src/store/initialState.js
+++ src/store/initialState.js
@@ -4,25 +4,7 @@ export default {
   app: {
     element: App,
     props: [['data', 'people', 1, 'name']],
-    children: [
-      {
-        element: 'p',
-        props: [],
-        children: ['p - Hellou']
-      },
-      {
-        element: App,
-        props: [['data', 'people', 0, 'name']],
-        children: [
-          'App - numero 2',
-          {
-            element: 'p',
-            props: [],
-            children: ['p - Moikka Maailma!']
-          }
-        ]
-      }
-    ]
+    children: []
   },
   data: {
     people: [
